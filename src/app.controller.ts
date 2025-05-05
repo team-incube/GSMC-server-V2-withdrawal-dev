27907976a67d078c4aab765808d973aa9d78cc48
snowykte0426 <snowykte0426@naver.com>
2025-05-05 22:20:13 +0900

    update: 불필요 파싱 제거

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Delete, Param, ParseIntPipe } from '@nestjs/common';
+import { Controller, Delete, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller('/api/v2/members')
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Delete('/withdraw/:studentCode')
-  withdraw(@Param('studentCode', ParseIntPipe) studentCode: number): any {
+  withdraw(@Param('studentCode') studentCode: string): any {
     return this.appService.withdraw(studentCode);
   }
 }

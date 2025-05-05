187482b9397bf46ba4173047aca8c9b2032b4b7e
snowykte0426 <snowykte0426@naver.com>
2025-05-05 22:20:01 +0900

    update: DB 연결정보 구성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,32 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AppController } from './app.controller';
+import { AppRepository } from './app.repository'; // ✅ 이거
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { StudentDetailEntity } from './entities/student-detail.entity';
+import { MemberEntity } from './entities/member.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'mysql',
+        host: config.get<string>('DB_HOST'),
+        port: parseInt(config.get('DB_PORT') || '3306', 10),
+        username: config.get<string>('DB_USERNAME'),
+        password: config.get<string>('DB_PASSWORD'),
+        database: config.get<string>('DB_DATABASE'),
+        entities: [StudentDetailEntity, MemberEntity],
+        synchronize: false,
+      }),
+    }),
+    TypeOrmModule.forFeature([StudentDetailEntity, MemberEntity]),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AppRepository],
 })
 export class AppModule {}
